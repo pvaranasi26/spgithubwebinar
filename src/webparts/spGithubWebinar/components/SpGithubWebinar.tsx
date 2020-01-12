0ddc56b276e61d160a2e2d99bf1fbddacee8c24b
praswa426 <pradeep.car26@gmail.com>
2020-01-12 14:02:36 -0500

    last trial

--- src/webparts/spGithubWebinar/components/SpGithubWebinar.tsx
+++ src/webparts/spGithubWebinar/components/SpGithubWebinar.tsx
@@ -10,7 +10,7 @@ export default class SpGithubWebinar extends React.Component<ISpGithubWebinarPro
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint Fx Azure Devops Demo!</span>
+              <span className={ styles.title }>Welcome to SharePoint Fx Azure Devops Demo- Last Trial!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>

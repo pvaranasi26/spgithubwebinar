77a2f4df1e9bc31eef617dd0e875472c82dc40ab
praswa426 <pradeep.car26@gmail.com>
2020-01-11 19:53:29 -0500

    initial commit fixes AB#1

---
+++ src/webparts/spGithubWebinar/components/SpGithubWebinar.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import styles from './SpGithubWebinar.module.scss';
+import { ISpGithubWebinarProps } from './ISpGithubWebinarProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class SpGithubWebinar extends React.Component<ISpGithubWebinarProps, {}> {
+  public render(): React.ReactElement<ISpGithubWebinarProps> {
+    return (
+      <div className={ styles.spGithubWebinar }>
+        <div className={ styles.container }>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <span className={ styles.title }>Welcome to SharePoint!</span>
+              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
+              <p className={ styles.description }>{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className={ styles.button }>
+                <span className={ styles.label }>Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
